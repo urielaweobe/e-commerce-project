39ddfc4e8d2964664f75ecc72854c1e0c9a1ffe2
URIEL <urielaweobe@gmail.com>
2022-03-03 16:05:32 +0100

    update:navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
-import { AppBar, ToolBar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { ShoppingCart } from '@material-ui/icons';
+import React from "react";
+import {
+  AppBar,
+  ToolBar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+
+import logo from "../../assets/commerce.png";
 
 const Navbar = () => {
   return (
     <>
-
-        <AppBar postion="fixed" className={classes.appBar} color='inherit'>
-            <ToolBar>
-                <Typography variant='h6' className={classes.title} color='inherit'>
-                    <img src={} alt="Commerce.js" height="25px" className={classes.image} />
-                    Commerce.js
-                </Typography>
-            </ToolBar>
-        </AppBar>
-
+      <AppBar postion="fixed" className={classes.appBar} color="inherit">
+        <ToolBar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            Commerce.js
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart item" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </ToolBar>
+      </AppBar>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
